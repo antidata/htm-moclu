edab3acf66acc1752e714fb64e6c6ccf1a5c1c46
antidata <antidata.05@gmail.com>
2015-09-02 00:27:41 -0500

    Adding network to HtmModel

--- src/main/scala/com/github/antidata/model/HtmModelEventData.scala
+++ src/main/scala/com/github/antidata/model/HtmModelEventData.scala
@@ -2,6 +2,6 @@ package com.github.antidata.model
 
 case class HtmModelEventData(modelId: HtmModelId, value: Double, timestamp: Long)
 
-case class HtmModel(id: HtmModelId, data: List[HtmModelData])
+case class HtmModel(id: HtmModelId, data: List[HtmModelData], network: HtmModelNetwork)
 
 case class HtmModelData(value: Double, timestamp: Long, anomalyScore: Option[Double])
