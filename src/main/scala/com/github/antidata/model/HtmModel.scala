d715f95fe3a1a9f27fd9bd2a21b0b6c62a60eeeb
antidata <antidata.05@gmail.com>
2015-09-24 23:35:52 -0500

    importing existentials implicits

--- src/main/scala/com/github/antidata/model/HtmModel.scala
+++ src/main/scala/com/github/antidata/model/HtmModel.scala
@@ -2,5 +2,6 @@ package com.github.antidata.model
 
 import org.numenta.nupic.network.Network
 import org.numenta.nupic.network.sensor.{Sensor, Publisher}
+import scala.language.existentials
 
 case class HtmModelNetwork(net: Network, publisher: Publisher, sensor: Sensor[_])
