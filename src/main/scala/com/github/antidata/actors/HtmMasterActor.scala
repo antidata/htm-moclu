6965c875a0fda4ecadddfee47c373d3d10770120
antidata <antidata.05@gmail.com>
2015-09-24 23:35:22 -0500

    MasterActor only handles the messages

--- src/main/scala/com/github/antidata/actors/HtmMasterActor.scala
+++ src/main/scala/com/github/antidata/actors/HtmMasterActor.scala
@@ -4,6 +4,8 @@ import akka.actor.{ActorLogging, Actor}
 import akka.cluster.sharding.ClusterSharding
 import scala.concurrent.duration._
 import com.github.antidata.actors.HtmModelActor._
+import akka.pattern.ask
+import akka.util.Timeout
 
 object HtmMasterActor {
 
@@ -11,21 +13,18 @@ object HtmMasterActor {
 
 class HtmMasterActor extends Actor with ActorLogging {
   import context.dispatcher
-  val tickTask = context.system.scheduler.schedule(3.seconds, 3.seconds, self, CreateHtmModel("123"))
   val htmModelsRegion = ClusterSharding(context.system).shardRegion(HtmModelActor.shardName)
+  implicit val timeout = akka.util.Timeout(10L, java.util.concurrent.TimeUnit.SECONDS)
 
   def receive = {
-    case e@CreateHtmModel(_) =>
-      htmModelsRegion ! e
-      self ! HtmModelEvent("123", HtmModelEventData("123", 9D, "7/2/10 0:00"))
-
-    case e@HtmModelEvent(_, _) =>
-      htmModelsRegion ! e
-
-    case e@CreateModelOk(id) =>
-      println(s"$id")
-
-    case e =>
-      log.debug(s"$e")
+    case event =>
+      log.info(s"Processing $event")
+      val thisSender = sender()
+      (htmModelsRegion ? event).mapTo[ClusterEvent]
+        .onSuccess {
+        case response =>
+          log.info(s"Repying $response")
+          thisSender ! response
+      }
   }
 }
