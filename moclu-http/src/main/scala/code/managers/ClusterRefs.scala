93125ef6bf9318d3e705f8673e8c6730172019b2
antidata <antidata.05@gmail.com>
2015-09-30 10:03:04 -0500

    Reading cluster port from config

--- moclu-http/src/main/scala/code/managers/ClusterRefs.scala
+++ moclu-http/src/main/scala/code/managers/ClusterRefs.scala
@@ -5,7 +5,9 @@ import com.github.antidata.bootstrap.Boot
 
 object ClusterRefs {
   lazy val actorSystem = {
-    Boot.startup(Seq("2551"))
+    import com.github.antidata.managers.AppConfiguration
+    val port = AppConfiguration.values.getString("akka.remote.netty.tcp.port")
+    Boot.startup(Seq(port))
     Boot.systemRef.actorOf(akka.actor.Props[HtmMasterActor], "master"+net.liftweb.util.StringHelpers.randomString(30))
   }
 }
